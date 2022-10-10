38964519946b05efedb4dd6aa0995de773cb66b8
xsuve <georgebaba99@yahoo.com>
2022-10-10 12:15:59 +0300

    Timeline updates

--- src/components/timeline/Timeline.tsx
+++ src/components/timeline/Timeline.tsx
@@ -20,13 +20,13 @@ export const Timeline: FC = () => {
     })();
   }, []);
 
-  const timelineLogColor = (status: TrackedJobStatus) => {
+  const timelineLogColor = (status: TrackedJobStatus): string => {
     switch (status) {
       case TrackedJobStatus.APPLIED:
         return 'red';
       case TrackedJobStatus.INTERVIEW:
         return 'yellow';
-        case TrackedJobStatus.OFFER:
+      case TrackedJobStatus.OFFER:
         return 'green';
       case TrackedJobStatus.TRACKING:
         return 'cyan';
@@ -38,7 +38,7 @@ export const Timeline: FC = () => {
   return (
     <div className='pt-20 px-6 pb-6 bg-gray-100'>
       <div className='mb-6 flex justify-between items-center'>
-        <h1 className='font-poppins font-normal text-xl'>Timeline</h1>
+        <h1 className='font-poppins font-normal text-xl'>Activitate</h1>
         <h6 className='font-poppins font-normal text-xs text-gray-400'>{moment().format('D MMMM, YYYY')}</h6>
       </div>
       <div className={`timeline ${timelineLogs.length > 0 ? 'has-logs' : ''} w-full h-full relative flex flex-col`}>
@@ -47,7 +47,9 @@ export const Timeline: FC = () => {
             timelineLogs.map((log, index)  => (
               <div key={index} className={`timeline-log timeline-log-color-${timelineLogColor(log.status)} relative px-5 py-4 rounded-lg ml-6 mb-4 ${Object.values(TrackedJobStatus).includes(log.status as TrackedJobStatus) ? 'bg-white drop-shadow-munk' : ''}`}>
                 <span className='font-poppins text-xs font-normal text-gray-400'>{moment(log.date).format('D MMMM, YYYY')}</span>
-                <h6 className='font-poppins text-sm font-normal mt-2 mb-1'>{log.title}</h6>
+                <a href={chrome.runtime.getURL('app.html')} target='_blank'>
+                  <h6 className='font-poppins text-sm font-normal mt-2 mb-1'>{log.title}</h6>
+                </a>
                 <p className='font-poppins text-sm font-normal text-gray-400'>{log.companyName}</p>
               </div>
             ))
